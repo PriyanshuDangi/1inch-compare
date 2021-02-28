13d356dbfd02e98801e1e9cf8b237330ec541175
Priyanshu Dangi <priyanshudangi37@gmail.com>
2021-03-01 00:31:06 +0530

    changes for heroku

---
+++ config/config-heroku.js
@@ -0,0 +1,10 @@
+const config = {
+    consumerKey: process.env.CONSUMERKEY,
+    consumerSecret: process.env.CONSUMERSECRET,
+    accessToken: process.env.ACCESSTOKEN,
+    accessTokenSecret: process.env.ACCESSTOKENSECRET,
+    // bearer_token: 'AAAAAAAAAAAAAAAAAAAAALvMMwEAAAAALRiiGniXn2l76McwDG3PC4zj8eU%3DsyzyA1BNWlGvwoD1ORzyzPCrscOMC9kWFmjHXuPVoiGAiAZbgm',
+    regularInterval: process.env.REGULARINTERVAL     //unit is in minutes
+  };
+  
+  module.exports = config;
